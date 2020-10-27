1033fa1995ff892df2fd27315dd9710d6f9703bc
Edouard BONLIEU <ebonlieu@gmail.com>
2020-10-27 09:33:48 +0100

    Responsiveness

--- components/instant-search.js
+++ components/instant-search.js
@@ -18,20 +18,9 @@ export const HitComponent = ({ hit, size }) => {
     <div className="flex flex-col bg-white rounded p-4 items-center shadow-lg">
       <img
         className="h-64 w-full object-cover"
-        src={hit?.imageUrl.replace('https:', 'http:')}
+        src={hit?.imageUrl.replace("https:", "http:")}
         alt={hit.display_name}
       />
-      <div class="flex justify-between mt-2">
-        {hit?.detectedLabels?.map((label, idx) => {
-          if (idx < 2) {
-            return (
-              <div class="pill bg-gray-400 rounded-full text-xs px-4 py-1 mr-2">
-                {label.Name}
-              </div>
-            );
-          }
-        })}
-      </div>
     </div>
   );
 };
@@ -42,15 +31,15 @@ HitComponent.propTypes = {
 
 const Hits = ({ hits, hasMore, refineNext, ...props }) => {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
       {hits.map((hit) => (
         <HitComponent key={`${hit.imageUrl}`} hit={hit} />
       ))}
 
-      <div className="col-span-3">
+      <div className="col-span-2 md:col-span-3">
         <button
           disabled={!hasMore}
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full"
           onClick={refineNext}
         >
           Load more
@@ -80,7 +69,7 @@ const SearchBox = ({ currentRefinement, refine }) => {
           </div>
           <div className="col-span-1">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  w-full"
               onClick={() => setIsUploadModalOpen(true)}
             >
               Upload
@@ -145,13 +134,13 @@ export default class extends React.Component {
         {...this.props}
       >
         <Configure hitsPerPage={100} />
-        <div className="container mx-auto m-8">
-          <div className="grid grid-cols-4 gap-4">
+        <div className="container mx-auto my-8 p-2">
+          <div className="grid grid-cols-4">
             <div className="col-span-4">
               <CustomSearchBox />
             </div>
             <div className="col-span-auto">
-              <div className="flex flex-col h-screen">
+              <div className="flex flex-col">
                 <div className="flex flex-col">
                   <h2 className="font-black text-2xl">Labels</h2>
                   <CustomRefinementList
@@ -161,7 +150,7 @@ export default class extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="row-span-1 col-span-3">
+            <div className="col-span-4 md:col-span-3">
               <CustomHits minHitsPerPage={100} />
             </div>
           </div>
